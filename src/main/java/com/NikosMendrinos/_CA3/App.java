ad07d78901c738f556f3cebf1a36c502a58b455c
nkmndr <36484744+nkmndr@users.noreply.github.com>
2018-03-01 11:21:37 +0000

    Initial commit

---
+++ src/main/java/com/NikosMendrinos/_CA3/App.java
@@ -0,0 +1,9 @@
+package com.NikosMendrinos._CA3;
+public class App {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
